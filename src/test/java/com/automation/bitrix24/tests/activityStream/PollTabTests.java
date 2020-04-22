7800238cacca450ff590cea543b1ea6c963ee482
mariialukianenko <lukianenko.mariia@gmail.com>
2020-04-22 15:24:07 -0500

    first commit pollTab

--- src/test/java/com/automation/bitrix24/tests/activityStream/PollTabTests.java
+++ src/test/java/com/automation/bitrix24/tests/activityStream/PollTabTests.java
@@ -3,6 +3,9 @@ package com.automation.bitrix24.tests.activityStream;
 import com.automation.bitrix24.pages.LoginPage;
 import com.automation.bitrix24.pages.activityStream.PollTab;
 import com.automation.bitrix24.tests.AbstractTestBase;
+import com.automation.bitrix24.utilities.BrowserUtils;
+import com.automation.bitrix24.utilities.Driver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -16,14 +19,15 @@ public class PollTabTests extends AbstractTestBase {
 
 
     /**
-     * US 4. As a user, I should be able to create a
-     * poll by clicking on Poll tab under Active Stream.
+     *  US 4. As a user, I should be able to create a
+     *  poll by clicking on Poll tab under Active Stream.
      */
 
 
     // AC 3. User should be able to attach link by clicking on the link icon.
+
     @Test
-    public void attachLink() {
+    public void attachLink(){
 
         test = report.createTest("Attach link by clicking on the link icon");
 
@@ -33,6 +37,7 @@ public class PollTabTests extends AbstractTestBase {
         PollTab pollTab = new PollTab();
         pollTab.navigateTo("Poll");
 
+        BrowserUtils.wait(3);
         pollTab.clickOnLinkIcon();
 
         String actual = pollTab.getPopupWindowHeader();
@@ -42,10 +47,56 @@ public class PollTabTests extends AbstractTestBase {
         pollTab.fillInTheFields();
         pollTab.clickOnSaveButton();
 
-        pollTab.verifyIfLinkWasAttached();
+        pollTab.switchToIFrame();
+        Assert.assertTrue(pollTab.returnText().isDisplayed());
+        pollTab.exitFromIFrame();
 
         test.pass("Link was attached.");
 
     }
+
+
+    // 5. User should be able to create a quote by clicking on the Comma icon.
+
+    @Test
+    public void createAQuote(){
+
+        test = report.createTest("Create a quote.");
+
+        LoginPage loginPage = new LoginPage();
+        loginPage.loginAsMarketing();
+
+        PollTab pollTab = new PollTab();
+        pollTab.navigateTo("Poll");
+
+        pollTab.sendQuote();
+        String expected = "Everyone's journey is different.";
+        String actual = pollTab.getPostedQuote().getText();
+        Assert.assertEquals(expected, actual);
+
+        test.pass("Quote posted.");
+    }
+
+
+    // 6. User should be able to add mention by clicking on the Add mention icon and select contacts from the lists provided in dropdown.
+
+    @Test
+    public void mentionContuct(){
+
+        test = report.createTest("Mention contacts by clicking on the Add mention icon");
+
+        LoginPage loginPage = new LoginPage();
+        loginPage.loginAsHr();
+
+        PollTab pollTab = new PollTab();
+        pollTab.navigateTo("Poll");
+
+        pollTab.selectContuctFromTheList();
+        Assert.assertTrue(pollTab.getMentionedContact().isDisplayed());
+
+        test.pass("User able to mention contact");
+    }
+
+
 
 }
