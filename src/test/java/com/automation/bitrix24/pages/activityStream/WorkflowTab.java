338f6a4482af8eb7ed7245a49ccacc04a630f35a
hakan-d <hakandurmus314@gmail.com>
2020-04-23 08:23:33 -0500

    pop up menu and upload files will be solved

--- src/test/java/com/automation/bitrix24/pages/activityStream/WorkflowTab.java
+++ src/test/java/com/automation/bitrix24/pages/activityStream/WorkflowTab.java
@@ -6,14 +6,19 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class WorkflowTab extends AbstractPageBase {
 
-    //Since we have wait object in PageBase class, we do not need to create it again here.
-//    protected WebDriverWait wait = new WebDriverWait(driver, 20);
+    @FindBy(css = "[id='blog-submit-button-save']")
+    private WebElement send;
+
+    @FindBy(css = "[id='blog-submit-button-cancel']")
+    private WebElement cancel;
 
     @FindBy(xpath = "//*[@type='text' and @name='NAME']")
     private WebElement gRequestsTitleTextBox;
@@ -42,11 +47,71 @@ public class WorkflowTab extends AbstractPageBase {
     @FindBy(css = "//*[text()='Select']")
     private WebElement gRequestsSelect;
 
-    @FindBy(css = "[id='blog-submit-button-save']")
-    private WebElement gRequestsSend;
+    @FindBy(xpath = "//button[@class='ui-btn ui-btn-lg ui-btn-primary' and text()='Send']")
+    private WebElement gRequestsSend2;
 
-    @FindBy(css = "[id='blog-submit-button-cancel']")
-    private WebElement gRequestsCancel;
+    @FindBy(css = "[class='feed-add-post-micro-title']")
+    private WebElement messagePlaceHolder;
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+    @FindBy(css = "[class='bx-lists-input-calendar']")
+    private WebElement lApprovalBothDates;
+
+    @FindBy(css = "[name='PROPERTY_88']")
+    private WebElement lApprovalAbsenceType;
+
+    @FindBy(css = "[name='PREVIEW_TEXT']")
+    private WebElement lApprovalReasonForLeave;
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    @FindBy(xpath = "//input[@type='text' and @name='NAME']")
+    private WebElement bTripTitle;
+
+    @FindBy(xpath = "//input[@type='text' and @name='PROPERTY_70[n0][VALUE]']")
+    private WebElement bTripDestination;
+
+    @FindBy(css = "[class='bx-lists-input-calendar']")
+    private WebElement bTripBothDates;
+
+    @FindBy(css = "[name='PREVIEW_TEXT']")
+    private WebElement bTripPurpose;
+
+    @FindBy(css = "[name='PROPERTY_73[n0][VALUE]']")
+    private WebElement bTripExpenses;
+
+    @FindBy(css = "[name='PROPERTY_74']")
+    private WebElement bTripCurrency;
+
+    @FindBy(css = "[name='PROPERTY_78[n0][VALUE]']")
+    private WebElement bTripAttDocs;
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    @FindBy(xpath = "//input[@type='text' and @name='NAME']")
+    private WebElement pRequestTitle;
+
+    @FindBy(css = "[name='PROPERTY_90[n0][VALUE]']")
+    private WebElement pRequestAmount;
+
+    @FindBy(css = "[name='PROPERTY_91']")
+    private WebElement pRequestCurrency;
+
+    @FindBy(css = "[name='PROPERTY_92[n0][VALUE]']")
+    private WebElement pRequestFile;
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    @FindBy(xpath = "//input[@type='text' and @name='NAME']")
+    private WebElement eReportTitle;
+
+    @FindBy(css = "[name='PROPERTY_79[n0][VALUE]']")
+    private WebElement eReportDescription;
+
+    @FindBy(css = "[name='PROPERTY_80[n0][VALUE]']")
+    private WebElement eReportAmount;
+
+    @FindBy(css = "[name='PROPERTY_81']")
+    private WebElement eReportCurrency;
+
+    @FindBy(css = "[name='PROPERTY_82']")
+    private WebElement eReportDocsAndReceipts;
+
+
+
 
     /**
      * choose submodules of Workflow:
@@ -117,11 +182,12 @@ public class WorkflowTab extends AbstractPageBase {
      */
     public void sendOrCancel(String sendOrCancel) {
         BrowserUtils.waitForPageToLoad(10);
-        BrowserUtils.scrollTo(gRequestsCancel);
+        BrowserUtils.scrollTo(cancel);
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        if (sendOrCancel.trim().equalsIgnoreCase("send")) gRequestsSend.click();
-        else if (sendOrCancel.trim().equalsIgnoreCase("cancel")) gRequestsCancel.click();
+        if (sendOrCancel.trim().equalsIgnoreCase("send")) send.click();
+        else if (sendOrCancel.trim().equalsIgnoreCase("cancel")) cancel.click();
         else throw new RuntimeException("no such button");
+        BrowserUtils.wait(4);
     }
 
 //  Common practice is not using explicit and implicit wait in same method. Choosing one of them is preferable.
@@ -130,14 +196,274 @@ public class WorkflowTab extends AbstractPageBase {
      */
     @Override
     public void navigateTo(String tabName) {
-        BrowserUtils.waitForPageToLoad(10);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         String tabNameXpath = "//span[contains(@class,'feed-add-post-form-')]/span[text()='" + tabName + "']";
         if (tabName.equals("File") || tabName.equals("Appreciation") || tabName.equals("Announcement") || tabName.equals("Workflow")) {
             WebElement moreTab = driver.findElement(By.cssSelector("[id=\"feed-add-post-form-link-more\"]"));
-            wait.until(ExpectedConditions.elementToBeClickable(moreTab)).click();
+            driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+            moreTab.click();
         }
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         WebElement tabElement = driver.findElement(By.xpath(tabNameXpath));
         wait.until(ExpectedConditions.elementToBeClickable(tabElement)).click();
     }
+
+    public void verifyRequestSent(){
+        Assert.assertEquals(messagePlaceHolder.getText(), "Send message …");
+    }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public void lEnterDate(String start, String end){
+        List<WebElement> bothDates = (List<WebElement>) lApprovalBothDates;
+        bothDates.get(0).sendKeys(start);
+        bothDates.get(1).click();
+        bothDates.get(1).sendKeys(end);
+    }
+
+    public void lSelectAbsenceType(String type){
+        int index;
+        switch (type.trim().toLowerCase()){
+            case "(not set)":
+                index=0;
+                break;
+            case "absent without reason or official leave":
+                index=1;
+            case "annual leave":
+                index=2;
+                break;
+            case "business trip":
+                index=3;
+                break;
+            case "maternity leave":
+                index=4;
+                break;
+            case "sick leave":
+                index=5;
+                break;
+            case "unpaid leave":
+                index = 6;
+                break;
+            case "personal calendars":
+                index = 7;
+                break;
+            case "other":
+                index = 8;
+                break;
+            default:
+                throw new RuntimeException("no such absence type");
+        }
+
+        Select absenceType = new Select(lApprovalAbsenceType);
+        absenceType.selectByIndex(index);
+    }
+
+    public void lApprovalEnterReasonForLeave(String reason){
+        lApprovalReasonForLeave.sendKeys(reason);
+    }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public void bTripEnterTitle(String title){
+        bTripTitle.sendKeys(title);
+    }
+
+    public void bTripEnterDestination(String destination){
+        bTripDestination.sendKeys(destination);
+    }
+
+    public void bTripEnterDate(String start, String end){
+        List<WebElement> bothDates = (List<WebElement>) bTripBothDates;
+        bothDates.get(0).sendKeys(start);
+        bothDates.get(1).click();
+        bothDates.get(1).sendKeys(end);
+    }
+
+    public void bTripEnterPurpose(String purpose){
+        bTripPurpose.sendKeys(purpose);
+    }
+
+    public void bTripEnterExpenses(String expense){
+        for (char c: expense.toCharArray()){
+            if (Character.isDigit(c)) throw new RuntimeException("Please enter only numbers");
+        }
+        bTripExpenses.sendKeys(expense);
+    }
+
+    public void bTripSelectCurrency(String currency){
+        int index;
+        switch (currency.trim().toUpperCase()){
+            case "(not set)":
+                index=0;
+                break;
+            case "USD":
+                index=1;
+            case "EUR":
+                index=2;
+                break;
+            case "GBP":
+                index=3;
+                break;
+            case "CNY":
+                index=4;
+                break;
+            case "JPY":
+                index=5;
+                break;
+            case "BRL":
+                index = 6;
+                break;
+            case "CAD":
+                index = 7;
+                break;
+            case "PLN":
+                index = 8;
+                break;
+            case "INR":
+                index = 9;
+                break;
+            case "AUD":
+                index = 10;
+                break;
+            default:
+                throw new RuntimeException("no such absence type");
+        }
+
+        Select currencyType = new Select(bTripCurrency);
+        currencyType.selectByIndex(index);
+    }
+
+    /***/
+    public void bTripAttachDocs(){
+//        bTripAttDocs
+    }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public void pRequestEnterTitle(String title){
+        pRequestTitle.sendKeys(title);
+    }
+
+    public void pRequestEnterAmount(String amount){
+        for (char c: amount.toCharArray()){
+            if (Character.isDigit(c)) throw new RuntimeException("Please enter only numbers");
+        }
+        pRequestAmount.sendKeys(amount);
+    }
+
+    public void pRequestSelectCurrency(String currency){
+        int index;
+        switch (currency.trim().toUpperCase()){
+            case "(not set)":
+                index=0;
+                break;
+            case "USD":
+                index=1;
+            case "EUR":
+                index=2;
+                break;
+            case "GBP":
+                index=3;
+                break;
+            case "CNY":
+                index=4;
+                break;
+            case "JPY":
+                index=5;
+                break;
+            case "BRL":
+                index = 6;
+                break;
+            case "CAD":
+                index = 7;
+                break;
+            case "PLN":
+                index = 8;
+                break;
+            case "INR":
+                index = 9;
+                break;
+            case "AUD":
+                index = 10;
+                break;
+            default:
+                throw new RuntimeException("no such absence type");
+        }
+
+        Select currencyType = new Select(pRequestCurrency);
+        currencyType.selectByIndex(index);
+    }
+
+    /***/
+    public void pRequestAddFile(){
+//        pRequestFile
+    }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public void eReportEnterTitle(String title){
+        eReportTitle.sendKeys(title);
+    }
+
+    public void eReportEnterDescription(String description){
+        eReportDescription.sendKeys(description);
+    }
+
+    public void eReportEnterAmount(String amount){
+        for (char c: amount.toCharArray()){
+            if (Character.isDigit(c)) throw new RuntimeException("Please enter only numbers");
+        }
+        eReportAmount.sendKeys(amount);
+    }
+
+    public void eReportSelectCurrency(String currency){
+        int index;
+        switch (currency.trim().toUpperCase()){
+            case "(not set)":
+                index=0;
+                break;
+            case "USD":
+                index=1;
+            case "EUR":
+                index=2;
+                break;
+            case "GBP":
+                index=3;
+                break;
+            case "CNY":
+                index=4;
+                break;
+            case "JPY":
+                index=5;
+                break;
+            case "BRL":
+                index = 6;
+                break;
+            case "CAD":
+                index = 7;
+                break;
+            case "PLN":
+                index = 8;
+                break;
+            case "INR":
+                index = 9;
+                break;
+            case "AUD":
+                index = 10;
+                break;
+            default:
+                throw new RuntimeException("no such absence type");
+        }
+
+        Select currencyType = new Select(eReportCurrency);
+        currencyType.selectByIndex(index);
+    }
+
+    /***/
+    public void eReportAddDocs(){
+//        eReportDocsAndReceipts
+    }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
 }
--- src/test/java/com/automation/bitrix24/tests/activityStream/WorkflowTabTests.java
+++ src/test/java/com/automation/bitrix24/tests/activityStream/WorkflowTabTests.java
@@ -3,26 +3,13 @@ package com.automation.bitrix24.tests.activityStream;
 import com.automation.bitrix24.pages.LoginPage;
 import com.automation.bitrix24.pages.activityStream.WorkflowTab;
 import com.automation.bitrix24.tests.AbstractTestBase;
-import com.automation.bitrix24.utilities.BrowserUtils;
 import com.automation.bitrix24.utilities.Driver;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import java.util.concurrent.TimeUnit;
 
 public class WorkflowTabTests extends AbstractTestBase {
 
-    //peer review:
-    //great working test, but have couple of issue as mention below :
-    //This test page is just for run test method. So we must not use driver.findElement method in this page
-    //we need to find locators in page class.
-    //Additionally; we use all wait on page class.
-
     protected WebDriver driver = Driver.getDriver();
-//    protected WebDriverWait wait = new WebDriverWait(driver, 20);
 
     /**
      * US8_AC3
@@ -37,18 +24,12 @@ public class WorkflowTabTests extends AbstractTestBase {
 
         loginPage.loginAsHelpDesk();
         workflowTab.workflowNavigation("General Requests");
-        //       driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         workflowTab.enterText("title", "mayor of the corner office");
-        //       driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         workflowTab.enterText("description", "I am sending this request regarding asdasdasd asdasdasd asdasdasd");
-        //      driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         workflowTab.priorityLevel("Low");
-        //      driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         workflowTab.enterText("send request to", "491");
-        //       driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         workflowTab.sendOrCancel("send");
 
-        BrowserUtils.wait(4);
-        Assert.assertEquals(driver.findElement(By.xpath("//span[text()='Send message …']")).getText(), "Send message …");
+        workflowTab.verifyRequestSent();
     }
 }
